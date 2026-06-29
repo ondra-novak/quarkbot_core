ea1f865c7ecc7cf9f1a37c8d9fa41d468ff45d73
Ondřej Novák <novak@cooberfund.com>
2026-06-29 21:24:31 +0200

    fix warning in gcc-14

--- src/ifc/context.hpp
+++ src/ifc/context.hpp
@@ -35,7 +35,7 @@ namespace quarkbot {
 
     class StrategyContext {
     public:
-        using Config = Config<std::function<std::optional<std::string_view>(const std::string &)> >;
+        using Config = ::quarkbot::Config<std::function<std::optional<std::string_view>(const std::string &)> >;
 
         ///List of tradable instruments available to the strategy
         /** the strategy can query for accounts and exchanges through the instruments */
